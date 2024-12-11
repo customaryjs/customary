139703e7a05551d576a8f0202bec48c0b3371693
André de Oliveira <andre@arbo.com.br>
2024-12-10 19:52:46 -0700

    customary-now.mjs including lit-for-customary

--- src/lit/CustomaryLitElement.ts
+++ src/lit/CustomaryLitElement.ts
@@ -1,5 +1,5 @@
 import {CustomaryLit} from "#customary/lit/CustomaryLit.js";
-import {html, LitElement, map, state} from "lit-for-customary";
+import {html, LitElement, map} from "lit-for-customary";
 import {CustomaryStateBroker} from "#customary/state/CustomaryStateBroker.js";
 import {AttributesMixin} from "#customary/attributes/AttributesMixin.js";
 import {EventsMixin} from "#customary/events/EventsMixin.js";
@@ -8,39 +8,44 @@ import {Mixin_firstUpdated} from "#customary/lifecycle/firstUpdated/Mixin_firstU
 export class CustomaryLitElement
 		extends AttributesMixin(EventsMixin(Mixin_firstUpdated(LitElement)))
 {
-	@state()
-	private state: any;
-
 	constructor() {
 		super();
-		this.state = CustomaryLit.getState(this);
+		(this as any).state = CustomaryLit.getState(this);
 	}
 
 	setState(state_or_fn: any) {
 		CustomaryStateBroker.setState(
-				state_or_fn, () => this.state, state => this.state = state
+				state_or_fn,
+				() => (this as any).state,
+				state => (this as any).state = state
 		);
 	}
 
 	protected override render(): unknown {
-		if (!map) throw new Error("https://www.typescriptlang.org/docs/handbook/modules/reference.html#type-only-imports-and-exports");
-
 		const template: HTMLTemplateElement = CustomaryLit.templateToRender(this);
 
-		const templateResult = this.interpolateVariables(template, html);
+		const templateResult = this.interpolateVariables(
+				template, (this as any).state, html, map
+		);
 
 		return html`${templateResult}`;
 	}
 
-	private interpolateVariables(template: HTMLTemplateElement, _html: any) {
+	private interpolateVariables(
+			template: HTMLTemplateElement,
+			_state: any,
+			_html: any,
+			_map: any
+	) {
 		const htmlFromTemplate = template.innerHTML;
 
 		// if the template has Lit directives with arrow functions,
 		// innerHTML will encode the '>' out of the '=>' so we need to decode it back
 		const htmlWithLitDirectives = htmlFromTemplate.replace('=&gt;', '=>');
 
-		const state = this.state;
+		const state = _state;
 		const html = _html;
+		const map = _map;
 
 		const s: string = `html\`${htmlWithLitDirectives}\``;
 
@@ -64,5 +69,7 @@ export class CustomaryLitElement
 		super.updated?.(changedProperties);
 	}
 
-	static properties: Record<PropertyKey, any> = {};
+	static properties: Record<PropertyKey, any> = {
+		state: {}
+	};
 }
