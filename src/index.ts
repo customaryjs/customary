6787cddba6e4fd07fd4308cfbe8140fac2b36881
André de Oliveira <andre@arbo.com.br>
2025-01-02 18:06:16 -0700

    Customary Testing

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,8 @@ import {CustomaryElement} from "#customary/CustomaryElement.js";
 
 // https://github.com/parcel-bundler/parcel/issues/4796#issuecomment-660356062
 // noinspection JSUnusedGlobalSymbols
+export type CustomaryDeclaration<T extends HTMLElement> = C<any>;
 export type CustomaryOptions<T extends HTMLElement> = C<any>;
 
 export * from "#customary/lit";
 export {Customary, CustomaryElement};
-
-// TODO customary-testing
-import {CustomaryTestingQueries} from "./testing/CustomaryTestingQueries.js";
-export {CustomaryTestingQueries};
