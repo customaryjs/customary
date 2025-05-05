230f1412d717f9692ea35eed3f38f0bf6ceb6847
André de Oliveira <andre@arbo.com.br>
2025-05-04 18:51:50 -0600

    lit 3.3.0

--- src/cssstylesheet/CSSStyleSheetImporter.ts
+++ src/cssstylesheet/CSSStyleSheetImporter.ts
@@ -2,52 +2,15 @@ import {FetchText, FetchText_DOM_singleton} from "#customary/fetch/FetchText.js"
 
 export class CSSStyleSheetImporter {
 
-    constructor(private readonly fetchText: FetchText = FetchText_DOM_singleton) {
-        this.importFn = getImportFn();
-    }
+    constructor(private readonly fetchText: FetchText = FetchText_DOM_singleton) {}
 
     async getCSSStyleSheet(location: string): Promise<CSSStyleSheet | undefined> {
-            if (this.importFn) {
-                return await this.importCSSStyleSheet(location, this.importFn);
-            }
-            return await this.fetchCSSStyleSheet(location);
-    }
-
-    private async importCSSStyleSheet(location: string, importFn: ImportFn) {
-        console.debug('import: ' + location);
-        try {
-            const cssModule: any = await importFn(location);
-            return cssModule.default;
-        }
-        catch (error) {
-            console.error(error);
-            return undefined;
-        }
+        return await this.fetchCSSStyleSheet(location);
     }
 
     private async fetchCSSStyleSheet(location: string) {
         const text: string = await this.fetchText.fetchText(location);
         const cssStyleSheet = new CSSStyleSheet({baseURL: location});
         return cssStyleSheet.replace(text);
-    }
-
-    private readonly importFn: ImportFn | undefined;
-}
-
-type ImportFn = (location: string) => Promise<CSSStyleSheet>;
-
-/**
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import/with
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules#loading_non-javascript_resources
- */
-function getImportFn(): ImportFn | undefined {
-    try {
-        return eval('location => import(location, {with: {type: "css"}})');
-    }
-    catch (error) {
-        if (error instanceof SyntaxError) {
-            return undefined;
-        }
-        throw error;
     }
 }
